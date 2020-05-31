1cddbc9d0f2b7cbd2e19e0dd3fc35caefce814e1
Adam <smolonek@gmail.com>
2020-05-31 20:35:46 +0200

    Updates

---
+++ precache-manifest.4d3efb072793b59add966dceb94f2514.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f404936c944576986987c983c891bb2f",
+    "url": "/hybrydki/index.html"
+  },
+  {
+    "revision": "871ac1e2e55c1314e0f5",
+    "url": "/hybrydki/static/css/2.ace36aee.chunk.css"
+  },
+  {
+    "revision": "d4f2e470bc8fa8ca6352",
+    "url": "/hybrydki/static/css/main.e29af139.chunk.css"
+  },
+  {
+    "revision": "871ac1e2e55c1314e0f5",
+    "url": "/hybrydki/static/js/2.3f99c1f0.chunk.js"
+  },
+  {
+    "revision": "46fa4528de05ecd6b197babccde18521",
+    "url": "/hybrydki/static/js/2.3f99c1f0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d4f2e470bc8fa8ca6352",
+    "url": "/hybrydki/static/js/main.3078c465.chunk.js"
+  },
+  {
+    "revision": "3eae4977659cfaca3d86",
+    "url": "/hybrydki/static/js/runtime-main.33fdb08e.js"
+  }
+]);
